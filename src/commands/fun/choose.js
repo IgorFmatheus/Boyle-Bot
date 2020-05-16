9c4a7808692cc0744debcce73d9535f8b6a7f375
IgorFmatheus <60252412+IgorFmatheus@users.noreply.github.com>
2020-05-16 16:44:38 -0300

    Choose command fix

--- src/commands/fun/choose.js
+++ src/commands/fun/choose.js
@@ -8,21 +8,24 @@ module.exports = {
   description: 'Choose an option between the args',
   usage: '<option | option>',
   execute: (client, message, args) => {
-
+    
+    
     if(args.length <= 2) return message.reply('I need more than 1 thing to choose between.').then(m => m.delete(5000));
-
+    
     let currentOption = '';
 
     let options = args.map( m => {
-      if(m === '|' && currentOption !== ''){
+      //[um, | , dois, | , tres]
+      if(m !== '|'){
+        currentOption += m;
+      }else{
         let aux = currentOption;
         currentOption = '';
         return aux;
-      }else if(m !== ','){
-        currentOption += m.replace(',', '');
       }
+      if(m === args[args.length - 1]) return currentOption;
     });
-
+    
     options = options.filter( m => { return m !== undefined});
 
     const choosen = options[Math.floor(Math.random() * options.length)];
